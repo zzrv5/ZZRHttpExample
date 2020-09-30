b8ba5aa927795db921f9afbf7c7882e38792cab8
ZZR <program.java@qq.com>
2020-10-01 07:52:59 +0800

    更新了访问前提示。

--- entry/src/main/java/com/zzrv5/http/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/zzrv5/http/slice/MainAbilitySlice.java
@@ -1,5 +1,6 @@
 package com.zzrv5.http.slice;
 
+import com.zzrv5.http.ResourceTable;
 import com.zzrv5.http.util.ZZRCallBack;
 import com.zzrv5.http.util.ZZRHttpUtil;
 import ohos.aafwk.ability.AbilitySlice;
@@ -16,43 +17,33 @@ import ohos.agp.utils.TextAlignment;
 
 public class MainAbilitySlice extends AbilitySlice {
 
-    private DirectionalLayout myLayout = new DirectionalLayout(this);
+
 
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
-        LayoutConfig config = new LayoutConfig(LayoutConfig.MATCH_PARENT, LayoutConfig.MATCH_PARENT);
-        myLayout.setLayoutConfig(config);
-        ShapeElement element = new ShapeElement();
-        element.setRgbColor(new RgbColor(255, 255, 255));
-        myLayout.setBackground(element);
-
-        Text text = new Text(this);
-        text.setLayoutConfig(config);
-        text.setText("Hello World");
-        text.setTextColor(new Color(0xFF000000));
-        text.setTextSize(50);
-        text.setTextAlignment(TextAlignment.CENTER);
-        myLayout.addComponent(text);
-        super.setUIContent(myLayout);
-
+        super.setUIContent(ResourceTable.Layout_layout);
+        Text text = (Text) findComponentById(ResourceTable.Id_text);
         text.setClickedListener(new Component.ClickedListener() {
             @Override
             public void onClick(Component component) {
+                text.setText("正在get访问,请稍后...");
                 String url="https://zzrv5.github.io/test/zzr.html";
                 ZZRHttpUtil.get(url, new ZZRCallBack.ZZRCallBackString() {
                     @Override
                     public void onFailure(int code, String errorMessage) {
                         //http访问出错了，注意此部分内容在主线程中工作，可以更新UI等操作，但请不要执行阻塞操作。
+                        text.setText("get访问出错："+errorMessage);
                     }
                     @Override
                     public void onResponse(String response) {
                         //http访问成功，注意此部分内容在主线程中工作，可以更新UI等操作，但请不要执行阻塞操作。
-                        text.setText("服务器页面内容为："+response);
+                        text.setText("服务器页面内容为（get）："+response);
                     }
                 });
             }
         });
+
     }
 
     @Override
